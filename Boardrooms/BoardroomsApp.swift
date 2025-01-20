89cc3963d192082f653e3b76b924b8ee79974b26
Monaalruthaya <monaalruthaya@outlook.com>
2025-01-20 14:52:57 +0300

    Initial Commit

---
+++ Boardrooms/BoardroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomsApp.swift
+//  Boardrooms
+//
+//  Created by mona alruthaya on 20/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BoardroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoardroomsTests/BoardroomsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoardroomsTests.swift
+//  BoardroomsTests
+//
+//  Created by mona alruthaya on 20/07/1446 AH.
+//
+
+import Testing
+@testable import Boardrooms
+
+struct BoardroomsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
